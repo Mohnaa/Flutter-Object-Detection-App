1904df590ab723eb727c4b7bf894cb8cf92be1fc
Mohnaa <mohnaag@gmail.com>
2024-03-20 00:39:52 +0530

    add files

---
+++ flutter_yolo_new/android/app/src/main/kotlin/com/example/flutter_yolo_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_yolo_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_yolo_new/lib/main_page.dart
@@ -0,0 +1,239 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:external_path/external_path.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:media_scanner/media_scanner.dart';
+class MainPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const MainPage({Key? key, required this.cameras}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late CameraController _cameraController;
+  late Future<void> _cameraValue;
+  List<File> _imagesList = [];
+  bool _isFlashOn = false;
+  int _selectedCameraIndex = 0; // 0 for rear camera, 1 for front camera
+
+  Future<File> _saveImage(XFile image) async {
+    final downloadPath = await ExternalPath.getExternalStoragePublicDirectory(
+        ExternalPath.DIRECTORY_DOWNLOADS);
+    final fileName = '${DateTime.now().millisecondsSinceEpoch}.png';
+    final file = File('$downloadPath/$fileName');
+    try {
+      await file.writeAsBytes(await image.readAsBytes());
+    } catch (_) {}
+
+    return file;
+  }
+
+  void _takePicture() async {
+    XFile? image;
+
+    if (_cameraController.value.isTakingPicture ||
+        !_cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (_isFlashOn == false) {
+      await _cameraController.setFlashMode(FlashMode.off);
+    } else {
+      await _cameraController.setFlashMode(FlashMode.torch);
+    }
+    image = await _cameraController.takePicture();
+
+    if (_cameraController.value.flashMode == FlashMode.torch) {
+      setState(() {
+        _cameraController.setFlashMode(FlashMode.off);
+      });
+    }
+
+    final file = await _saveImage(image);
+    setState(() {
+      _imagesList.add(file);
+    });
+    MediaScanner.loadMedia(path: file.path);
+  }
+
+  void _startCamera(int cameraIndex) {
+    _cameraController = CameraController(
+      widget.cameras[cameraIndex],
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+    _cameraValue = _cameraController.initialize();
+    _selectedCameraIndex = cameraIndex;
+  }
+
+  Future<void> _switchCamera() async {
+    int newCameraIndex = _selectedCameraIndex == 0 ? 1 : 0;
+    await _cameraController.dispose();
+    _startCamera(newCameraIndex);
+  }
+
+  @override
+  void initState() {
+    _startCamera(0); // Start with the rear camera
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromRGBO(255, 255, 255, .7),
+        shape: const CircleBorder(),
+        onPressed: _takePicture,
+        child: const Icon(
+          Icons.camera_alt,
+          size: 40,
+          color: Colors.black87,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: _cameraValue,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return SizedBox(
+                  width: size.width,
+                  height: size.height,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: size.width,
+                      child: CameraPreview(_cameraController),
+                    ),
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          SafeArea(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 5, top: 10),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isFlashOn = !_isFlashOn;
+                        });
+                      },
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(50, 0, 0, 0),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: _isFlashOn
+                              ? const Icon(
+                                  Icons.flash_on,
+                                  color: Colors.white,
+                                  size: 30,
+                                )
+                              : const Icon(
+                                  Icons.flash_off,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                        ),
+                      ),
+                    ),
+                    const Gap(10),
+                    GestureDetector(
+                      onTap: _switchCamera,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(50, 0, 0, 0),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: _selectedCameraIndex == 0
+                              ? const Icon(
+                                  Icons.camera_rear,
+                                  color: Colors.white,
+                                  size: 30,
+                                )
+                              : const Icon(
+                                  Icons.camera_front,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 7, bottom: 75),
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: _imagesList.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(2),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image(
+                                height: 100,
+                                width: 100,
+                                opacity: const AlwaysStoppedAnimation(07),
+                                image: FileImage(
+                                  _imagesList[index],
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
